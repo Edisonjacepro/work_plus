191b870a8e7bc866b61ec3905c461c8223193676
edison <edison.jace.pro@gmail.com>
2026-02-11 16:35:23 +0100

    feat: add offer CRUD

---
+++ src/Controller/OfferController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Offer;
+use App\Form\OfferType;
+use App\Repository\OfferRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/offers')]
+class OfferController extends AbstractController
+{
+    #[Route('', name: 'offer_index', methods: ['GET'])]
+    public function index(OfferRepository $offerRepository): Response
+    {
+        return $this->render('offer/index.html.twig', [
+            'offers' => $offerRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'offer_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $offer = new Offer();
+        $form = $this->createForm(OfferType::class, $offer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($offer);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('offer_show', ['id' => $offer->getId()]);
+        }
+
+        return $this->render('offer/new.html.twig', [
+            'offer' => $offer,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'offer_show', methods: ['GET'])]
+    public function show(Offer $offer): Response
+    {
+        return $this->render('offer/show.html.twig', [
+            'offer' => $offer,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'offer_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Offer $offer, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(OfferType::class, $offer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('offer_show', ['id' => $offer->getId()]);
+        }
+
+        return $this->render('offer/edit.html.twig', [
+            'offer' => $offer,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'offer_delete', methods: ['POST'])]
+    public function delete(Request $request, Offer $offer, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete_offer_' . $offer->getId(), (string) $request->request->get('_token'))) {
+            $entityManager->remove($offer);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('offer_index');
+    }
+}
