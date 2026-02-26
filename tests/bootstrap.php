dbd4e50015e3a3fae6d8675676d8688327815609
edison <edison.jace.pro@gmail.com>
2026-02-26 16:47:32 +0100

    chore: switch to strict env template mode

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,9 +5,17 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    $projectDir = dirname(__DIR__);
+    $dotenvPath = $projectDir.'/.env';
+    if (!is_file($dotenvPath)) {
+        $dotenvPath = $projectDir.'/.env.dist';
+    }
+
+    if (is_file($dotenvPath)) {
+        (new Dotenv())->bootEnv($dotenvPath);
+    }
 }
 
-if ($_SERVER['APP_DEBUG']) {
+if ((bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false)) {
     umask(0000);
 }
